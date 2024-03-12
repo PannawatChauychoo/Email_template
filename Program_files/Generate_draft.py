3c584e2abfc65e92498f31598e65425cc139a7b4
Pannawat <pchauych@usc.edu>
2024-03-12 13:37:14 -0700

    Track Generate_draft.py

--- Program_files/Generate_draft.py
+++ Program_files/Generate_draft.py
@@ -10,6 +10,7 @@ import argparse
 import base64
 from email.message import EmailMessage
 
+
 import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
