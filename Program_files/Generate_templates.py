e5c7aed23ffc6a541e6199abc8d9b07588822210
Pannawat <pchauych@usc.edu>
2024-03-18 08:33:01 -0700

    Email template generate in html format

--- Program_files/Generate_templates.py
+++ Program_files/Generate_templates.py
@@ -36,70 +36,48 @@ def write_email(target_info, your_info):
 
 
    if follow_up_count == 0:
-      print("1")
-      Email = f"""Hello {target_name}, 
-      I hope you are having a wonderful week! 
-      My name is {name}, and I’m currently a rising business senior at USC while pursuing a master in {PDP}. 
-      {firm} is my top choice for a {position} next year and I hope I can ask you for more advice on the process given our shared interest in {target_common_interest}. 
-      I believe I would hit the ground running with my experience in research and leadership. For your convenience, I have listed some of my availabilities below:
-      
-      · {availabilities[0]}
-      · {availabilities[1]}
-      · {availabilities[2]}
-      
-      I understand you’re very busy and that these times might not work for you. If that’s the case, please let me know and I’d be happy to work around your schedule. 
-      I have also attached my resume below, which shows the relevant internships I’ve had to date. Thank you for your time and consideration. I look forward to hearing from you and have a wonderful day. 
+      print("1st_Template")
+      Email = f"""Hello {target_name},
+<p>I hope you are having a wonderful week! </p>
+<p>My name is {name}, and I’m currently a rising business senior at USC while pursuing a master in {PDP}. {firm} is my top choice for a {position} next year and I hope I can ask you for more advice on the process given our shared interest in {target_common_interest}. I believe I would hit the ground running with my experience in research and leadership. For your convenience, I have listed some of my availabilities below: </p>
+<ul>
+   <li>{availabilities[0]}</li>
+   <li>{availabilities[1]}</li>
+   <li>{availabilities[2]}</li>
+</ul>
+<p>I understand you’re very busy and that these times might not work for you. If that’s the case, please let me know and I’d be happy to work around your schedule. </p>
+<p>I have also attached my resume below, which shows the relevant internships I’ve had to date. Thank you for your time and consideration. I look forward to hearing from you and have a wonderful day. </p>
 
-      Cheers,
-         {name}"""
+<p>Cheers, </p>
+{name}"""
       
 
    elif follow_up_count == 1:
-      print("2")
-      Email = f""" Hi {target_name}, 
-      I hope your week is going well. I emailed last week and just wanted to reach out again since my email probably got lost in your inbox. I think you're a very accomplished person and feel that I can learn a lot from speaking with you. Please let me know if you have 5-10 minutes for a quick chat. No pressure at all if you’re too busy, but I would really appreciate it and am happy to work around your busy schedule.
-      Or if it’s easier for you, I’ve also included some of my availabilities below:
-      
-      · {availabilities[0]}
-      · {availabilities[1]}
-      · {availabilities[2]}
-      
-      Please feel free to let me know either way. Thanks for your consideration and I look forward to hopefully connecting soon.
-      
-      Cheers,
-      {name}"""
+      print("2nd_Template")
+      Email = f"""Hi {target_name},
+<p>I hope your week is going well! </p>
+<p>I emailed last week and just wanted to reach out again since my email probably got lost in your inbox. I think you're a very accomplished person and feel that I can learn a lot from speaking with you. Please let me know if you have 5-10 minutes for a quick chat. No pressure at all if you’re too busy, but I would really appreciate it and am happy to work around your busy schedule.</p>
+<p>Or if it’s easier for you, I’ve also included some of my availabilities below:</p>
+<ul>
+    <li>{availabilities[0]}</li>
+    <li>{availabilities[1]}</li>
+    <li>{availabilities[2]}</li>
+</ul>
+<p>Please feel free to let me know either way. Thanks for your consideration and I look forward to hopefully connecting soon.</p>
+<p>Cheers, </p>
+{name}"""
       
    elif follow_up_count == 2:
-      print("3")
+      print("3rd_Template")
       Email = f"""Hi {target_name},
-      I hope your week is going well. I emailed last week and just wanted to reach out again since my email probably got lost in your inbox. 
-      I think you're a very accomplished person and feel that I can learn a lot from speaking with you. Please let me know if you have 5-10 minutes for a quick chat. No pressure at all if you’re too busy, but I would really appreciate it and am happy to work around your busy schedule.
-
-      Or if it’s easier for you, I’ve also included some of my availabilities below:
-      · {availabilities[0]}
-      · {availabilities[1]}
-      · {availabilities[2]}
-      
-      Please feel free to let me know either way. Thanks for your consideration and I look forward to hopefully connecting soon.
-      
-      Cheers,
-      {name}"""
+<p>I hope your week is going well! </p>
+<p>I reached out over a week ago hoping for a chance to learn more about yourself & your experience at {firm}. I’m sure you can tell I’m very eager to speak with you; however, I wanted to make sure I’m striking the right balance between being persistent and being respectful, so if now is not a good time I will stop reaching out for now.</p>
+<p>If you can spare 10 minutes of your time at all, I would really appreciate it. Even if it's just a few minutes, I would be grateful to just get some general career advice from you.</p>
+<p>Thank you in advance for your time and have a great day.</p>
+<p>Cheers,</p>
+{name}"""
    
-   elif follow_up_count == 3:
-      print("4")
-      Email = f"""Hi {target_name},
-      I hope your week is going well. I reached out over a week ago hoping for a chance to learn more about yourself & your experience at Barclays. 
-      I’m sure you can tell I’m very eager to speak with you; however, I wanted to make sure I’m striking the right balance between being persistent and being respectful, so if now is not a good time I will stop reaching out for now.
-      If you can spare 10 minutes of your time at all, I would really appreciate it. Even if it's just a few minutes, I would be grateful to just get some general career advice from you.
-
-      Thanks in advance for your time and have a great day.
-
-      Cheers,
-      {name} """        
-   
-   #cp.copy(str(Email))
-   
-   return Email #print("\n Copied into your clipboard! You can paste this anywhere now.")  
+   return Email 
    
 
       
